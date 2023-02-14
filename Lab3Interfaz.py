23e6ac3112208b02131fa7eab727f29c40c56c24
hector073 <124469103+hector073@users.noreply.github.com>
2023-02-14 11:35:47 -0600

    Funcionan la interfaz y  la com por separado
    Si no se ejecuta la interfaz, la comunicacion de arduino a py funciona bien, pero si corro ambos, la interfaz no responde, asi que cmente la interfaz

--- Lab3Interfaz.py
+++ Lab3Interfaz.py
@@ -1,10 +1,51 @@
+#########################################Links#########################################################
+#video sobre como leer datos de arduino en py: https://www.youtube.com/watch?v=AHr94RtMj1A
+
 #########################################IMPORTS######################################################
 
 from tkinter import * #el * va a importar todo de la libreria de tkinter
 
+import serial.tools.list_ports #para importar esta libreria es necesario instalar la libreria. Para ello 
+# se escribe pip install pyserial en el comand prompt y se oprime enter
 
-####################################CION DE LA INTERFAZ###############################################
+def comArduinoAPython():   
+    #########################################Definicion de variables y arrays##############################
 
+    ports = serial.tools.list_ports.comports() # enlista los puertos com utilizados
+    serialInst = serial.Serial()  
+
+    portsList = [] #array en el que se guardaran los puertos com para luego ser desplegados
+
+    #####################################COM PORTS##########################################################
+
+    for onePort in ports:#este ciclo for despliega en pantalla los puertos com disponibles
+        portsList.append(str(onePort))
+        print(str(onePort)) 
+
+    val = input("Seleccione Puerto: COM") #el usuario puede seleccionar el puerto due que quiera
+    #recibir datos
+
+    #ahora queremos toma el numero seleccionado por el usuario y unirlo a un string que pueda identificar 
+    # nuestro dispositivo serial
+    for x in range(0,len(portsList)):
+        if portsList[x].startswith("COM" + str(val)):
+            portVar = "COM" + str(val)
+            print(portVar)
+
+
+
+    #---------------------------Seteando nuestro dispositivo serial para poder abrirlo--------------
+    serialInst.baudrate = 9600
+    serialInst.port = portVar
+    serialInst.open()
+
+    while True:#se crea un loop para recibir datos constantemente
+        if serialInst.in_waiting:            
+            packet = serialInst.readline()
+            print(packet.decode('utf').rstrip('\n'))
+"""
+####################################CREACION DE LA INTERFAZ###############################################
+#def Interfaz():
 root = Tk() #Toplevel widget of Tk which represents mostly the main window of an application. 
 #It has an associated Tcl interpreter.
 root.title("Monitoreo de sensores") #cambia el titulo de la ventana
@@ -58,4 +99,15 @@ boton5min.grid(row = 6, column = 0)
 boton10min.grid(row = 7, column = 0)
 
 
+
+
+#Interfaz()   
+#comArduinoAPython()
+
+
+
+root.after(0, comArduinoAPython) #https://pythonguides.com/python-tkinter-mainloop/   
 root.mainloop() #crea el loop 
+"""
+
+comArduinoAPython()
