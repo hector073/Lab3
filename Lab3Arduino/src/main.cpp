5eea4abc1371e625193bcd80782f4fa5cb5ddf4f
hector073 <124469103+hector073@users.noreply.github.com>
2023-02-15 13:54:19 -0600

    updateArduino

--- Lab3Arduino/src/main.cpp
+++ Lab3Arduino/src/main.cpp
@@ -12,17 +12,41 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
 */
 
+String msg;
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+
+}
+//**************************Funciones*****************************
+void frecuenciaMuestreo(){
+//esta funcion sirve para leer el dato enviado desde python e interpretarlo para luego 
+//cambiar la frecuencia de muestreo
+  if(Serial.available() > 0){
+    msg = Serial.readString();
+  }
+  
+  if(msg == "uno"){
+
+  }
+
+  else if(msg == "cinco"){
+
+  }
+
+  else if(msg == "diez"){
+    
+  }
+
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
+  // read the input on analog pin 36:
   int sensorValue = analogRead(36);
   // print out the value you read:
   Serial.println(sensorValue);
   delay(100);  // delay in between reads for stability
 }
+
